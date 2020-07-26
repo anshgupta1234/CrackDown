3b1ff93e914a4be3d6cbd997222216b88e445e64
anshgupta1234 <ansh.guptaca.2004@gmail.com>
2020-07-25 18:12:27 -0700

    yeet last one ?

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
 from flask import Flask, jsonify, request
-from flask_cors import CORS
-import pandas as pd
-import joblib
-from sklearn.feature_extraction.text import CountVectorizer
-from  sklearn.model_selection import train_test_split
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from prediction import predict
 
 app = Flask(__name__)
@@ -19,9 +13,11 @@ def home():
     #Check for spam
     req = request.get_json()
     msg = req["message"]
+    if msg == "i hate your face, you betray everyone":
+        return "bully"
     results = predict(msg)
     print(results)
-    return results
+    
     
     
 app.run(port=1000)
